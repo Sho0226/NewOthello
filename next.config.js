9552ad1e28db2065d0813e2face0a77d9a292bec
Sho0226 <s1F102201616@iniad.org>
2024-06-10 00:22:53 +0900

    Prettier

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  basePath:
-    process.env.GITHUB_REPOSITORY !== undefined
-      ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
-      : '',
+  basePath: process.env.GITHUB_REPOSITORY !== undefined ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '',
   output: 'export',
   trailingSlash: true,
   eslint: { ignoreDuringBuilds: true },
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recess-order',
-    'stylelint-prettier/recommended',
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order', 'stylelint-prettier/recommended'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
